5fff713d5c25508c02808042c769971fd8999318
Umer Khan <91822670+Umer-Khan-00000@users.noreply.github.com>
2024-09-23 13:20:10 +0500

    Completed 5 Files

--- Exception Handling.py
+++ Exception Handling.py
@@ -1,12 +1,20 @@
-a = input("Enter the number: ")
-print(f"Multiplication table of {a} is: ")
-
+# a = input("Enter the number: ")
+# print(f"Multiplication table of {a} is: ")
 # try:
+#   for i in range(1, 11):
+#     print(f"{int(a)} X {i} = {int(a)*i}")
+# except:
+#   print("Invalid  Input!")
 
-for i in range(1, 11):
-        print(f"{int(a)} X {i} = {int(a)*i}")
+# print("Some imp lines of code")
+# print("End of program")
 
-# except Exception as e:
-#     print(e)
-
+try:
+    num = int(input("Enter an integer: "))
+    a = [6, 3]
+    print(a[num])
+except ValueError:
+    print("Number entered is not an integer.")
     
+except IndexError:
+  print("Index Error")
