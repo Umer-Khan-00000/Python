aa8e6adac5fa0b2027a25fa0fa9083e25addb6a5
Your Name <you@example.com>
2024-10-20 00:32:28 +0500

    Learned File Handling

--- Day 49 - File IO/File IO.py
+++ Day 49 - File IO/File IO.py
@@ -1,4 +1,18 @@
-f = open('File.txt', 'r')
+
+# Reading the file :
+
+f = open('Day 49 Example.txt', 'r')
 text = f.read()
 print(text)
 f.close()
+
+# Writing to a file :
+
+f = open('Day 49 Example.txt', 'a')
+f.write("This is a new line added by me.\n")
+f.close()
+
+with open('Day 49 Example.txt', 'a') as f:
+  f.write("This is a new line added by me")
+
+ 
