3e8fb37e749616cee981402d580375a306eb034a
Your Name <you@example.com>
2024-11-03 01:10:38 +0500

    Worked on SEEK() Functions

--- seek() & tell() methods.py
+++ seek() & tell() methods.py
@@ -1,5 +1,27 @@
 
-with open('Day 51 Example.txt', 'r') as f:
-    print(type(f))
-    # Moves the cursor after 10th bytes in the file
-    f.seek(10)
+# -------------------------------SEEK() & TELL() METHODS--------------------------
+
+# -------------------------------EXAMPLE NO. 01 :--------------------------
+
+
+# with open('Day 51 Example.txt', 'r') as f:
+#     print(type(f))
+#     # Moves the cursor after 10th bytes in the file
+#     f.seek(10)
+
+#     # Read the next 5 bytes :
+#     data = f.read(5)
+    # print(data)
+
+# -------------------------------EXAMPLE NO. 02 :--------------------------
+
+with open('Day 51 Example2.txt', 'w') as f:
+    f.write("Hello World3 !")
+    f.truncate(3)
+
+with open('Day 51 Example2.txt', 'r') as f:
+    print(f.read())    
+
+
+
+
