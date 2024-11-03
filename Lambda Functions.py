23deed56ff84263290c705cf677b6c9324a5b346
Umer Khan <umerwork3931@gmail.com>
2024-11-04 00:03:17 +0500

    Add files via upload

---
+++ Lambda Functions.py
@@ -0,0 +1,15 @@
+
+            # ******^^^****** :: LAMBDA FUNCTIONS :: ******^^^******
+
+# Making functions --->
+double = lambda x: x*2
+cube = lambda x: x*x*x
+avg = lambda x,y: (x + y)/2
+
+# Print the functions --->
+print(double(5))
+print(cube(5))
+print(avg(5, 5))
+
+
+
