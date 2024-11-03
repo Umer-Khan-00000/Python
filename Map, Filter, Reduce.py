474a315b87f92f360c0cbe4af97417d384c25106
Your Name <umerwork3931@gmail.com>
2024-11-04 00:53:44 +0500

    Working on filter function ...

--- Map, Filter, Reduce.py
+++ Map, Filter, Reduce.py
@@ -1,4 +1,6 @@
-# Cube Function --->
+                    # ^^^^^***^^^^^ MAP Methods / Functions ^^^^^***^^^^^
+
+# Function to cube a number --->
 def cube(x):
     return x*x*x
 
@@ -17,8 +19,20 @@ for items in List:
 # Short Function Step (using map) --->
 # So Use this instead of using long for loops --->
 
+print("\nThis is an output for map() function :")
 NewList = list(map(cube, List))  # Using map function instead of for loop.
 
 # Print all Cubes in a NewList --->
 print(NewList)
+
+                # ^^^^^***^^^^^ FILTER Methods / Functions ^^^^^***^^^^^
+
+# Function to filter --->
+def filter_func(a):
+      return a>2
+
+print("\nThis is an output for Filter Function :")
+List2 = list(filter(filter_func, List))
+print(List2)                
+
 
