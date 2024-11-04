fe2193441060ad7ddf616925afb618b1821e2bc0
Your Name <umerwork3931@gmail.com>
2024-11-05 01:27:16 +0500

    Created New File and Worked on Classes & Objects ...

---
+++ Classes & Objects.py
@@ -0,0 +1,17 @@
+                            # ~~~ CLASSES AND OBJECTS  ~~~
+
+# Making a class named Person --->
+class Person :
+    Name = "Umer"
+    Occupation = "Python Developer"
+    NetWorth = 125000
+
+# Creating a method (using self parameter) --->
+def info(self):
+    print(f"{self.Name} is a {self.Occupation}")
+
+# Calling the method --->
+a = Person()
+a.Name = "Mustafa"
+a.Occupation = "Doctor"
+a.info()
