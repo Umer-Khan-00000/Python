19204266b296ff7ea27e61efdd1285263ef23415
Your Name <umerwork3931@gmail.com>
2024-11-04 23:26:28 +0500

    Wrote Scenarios for Project 5

--- All Projects & Exercises/Snake, Water & Gun.py
+++ All Projects & Exercises/Snake, Water & Gun.py
@@ -5,10 +5,11 @@
 # ~ Do Not Create a Fancy GUI
 # ~ Use Proper Functions to check for a Win ...
 
+# Scenarios --->
 
-                         # S W G
-# ~ Computer =             0 1 2
+                    # S W G
+# ~ Computer =        0 1 2
+# ~ Player   =  S 0   D W L
+#               W 1   L D W
+#               G 2   W L D
 
-# ~ Player   =  S  0       D W L
-#               W  1       L D W
-#               G  2       W L D
