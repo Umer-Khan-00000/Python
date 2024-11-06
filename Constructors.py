4afac856d48af3dd4f6fcd1634ee293244a411ba
Your Name <umerwork3931@gmail.com>
2024-11-07 01:49:17 +0500

    Workes on Constructors in OOP's ....

---
+++ Constructors.py
@@ -0,0 +1,15 @@
+                        # ~~~ Constructors in Pythob ~~~
+
+# Example for assinging variables and Printing from a Function --->
+
+class Person :
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print("\nExample for assinging variables and Printing from a Method ---> ")
+        print(f"My name is {self.name} and my age is {self.age}")    
+
+a = Person("Umer", 15)
+a.info()
