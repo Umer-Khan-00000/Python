a4bdc2c52f96af1888885a323a36b2ba28262878
Your Name <umerwork3931@gmail.com>
2024-11-12 00:16:29 +0500

    Example ...

--- Getters & Setters.py
+++ Getters & Setters.py
@@ -1,4 +1,16 @@
 class MyClass:
     def __init__(self, value):
-        self.value = value
+        self._value = value
     def show(self):
+        print(f"Value is: {self._value}")
+    @property
+    def value(self):
+        return self._value
+
+
+obj = MyClass(10)
+print(obj._value)
+obj.show()
+
+
+
