43a299da135ab6cb1a6de2be5052d12a13340e91
Your Name <umerwork3931@gmail.com>
2024-11-18 00:47:33 +0500

    Updated ...

--- Decorators.py
+++ Decorators.py
@@ -1,21 +1,14 @@
 def greet(fx):
-    def mfx():
-        print("Good Morning")
-        fx()
-         # {
-            # @greet
-            # def hello():
-            #     print("Hello, World!")
-        # }
-
-        print("Thanks for using this ...")
-    return mfx
+    print("Good Morning !")
+    fx()
+    # {
+    #     @greet
+    #     def hello():
+    #     print("Hello, World!")
+    # }
+    print("Thanks for using this function!")
+    return greet
 
 @greet
 def hello():
     print("Hello, World!")
-
-def add(a, b):
-    print(a + b)
-
-hello() 
--- Getters & Setters.py
+++ Getters & Setters.py
@@ -12,7 +12,8 @@ obj = MyClass(10)
 print(obj._value)
 obj.show()
 
-
+mfk = MyClass
+memoryview
 
 
 
